38623eb139a466ca6d8a2ff7f823d5a2a468aaad
Siddhant Baroth <69706232+Officially-Siddhant@users.noreply.github.com>
2025-04-27 21:37:48 -0400

    Update WeightedTopologyController.cpp
    Misuse of comma operator, replaced with &&. Handled the compiler warning.

--- src/SwarmControllers/WeightedTopology/WeightedTopologyController.cpp
+++ src/SwarmControllers/WeightedTopology/WeightedTopologyController.cpp
@@ -1,145 +1,147 @@
 /**
- * @brief Controller implementing distributed leader-follower formation control for multiple quadrotors with weighted topology.
- *
- * This controller is based on the paper:
- *
- * "Distributed leader-follower formation control for multiple quadrotors with weighted topology"
- * Zhicheng Hou, Isabelle Fantoni, hal-01180491
- *
- * It extends the SwarmController class to implement specific behavior for the weighted topology scenario.
- *
+ * @brief WeightedTopologyNeighbors class for calculating weighted topology neighbors based on vehicle positions.
+ * @details This class extends the NearestNeighbors class template to calculate weighted topology neighbors.
+ * It provides functionality to process neighbor positions, enrich neighborhoods, and handle weighted topology neighbors.
  * @author Arthur Astier
  */
 
-#include "SwarmControllers/WeightedTopology/WeightedTopologyController.hpp"
+#include "SwarmControllers/WeightedTopology/WeightedTopologyNeighbors.hpp"
 
 /**
- * @brief Constructs a new Weighted Topology Controller object.
- *
- * Initializes the controller by retrieving gains from parameters.
- */
-Controller::WeightedTopologyController::WeightedTopologyController() : SwarmController() {
-    this->declare_parameter<std::vector<double>>("gains");
-
-    const auto gains{this->get_parameter("gains").as_double_array()};
-    if (std::size(gains)!=9){
-        RCLCPP_INFO(this->get_logger(),"You didn't provide Kp, Ki, Kd for each PID (9 gains to provide)");
-    }else{
-        pid_ax.setKp(gains[0]);
-        pid_ax.setKi(gains[1]);
-        pid_ax.setKd(gains[2]);
-        pid_ay.setKp(gains[3]);
-        pid_ay.setKi(gains[4]);
-        pid_ay.setKd(gains[5]);
-        pid_az.setKp(gains[6]);
-        pid_az.setKi(gains[7]);
-        pid_az.setKd(gains[8]);
-    }
-
-}
-
-/**
- * @brief Convert neighbors data to matrix representation.
- *
- * @param neighbors The neighbors data received from the subscription.
- */
-void Controller::WeightedTopologyController::neighbors_to_matrix(
-        const WeightedTopologyController::WeightedTopologyNeighbors &neighbors) {
-    const auto neighbors_position{neighbors.neighbors_position};
-    const auto weights{neighbors.weights};
-    neighborhood = Neighborhood::Zero(6, std::size(neighbors_position));
-    std::for_each(std::begin(neighbors_position), std::end(neighbors_position),
-                  [&, idx = 0u](const auto &position) mutable {
-                      const PoseTwist neighborPoseTwist{position.x, position.vx, position.y, position.vy, position.z,
-                                                        position.vz};
-                      neighborhood.block(0, idx, 6, 1) = weights[idx] * neighborPoseTwist;
-                      ++idx;
-                  });
-}
-
-/**
- * @brief Callback function for processing incoming neighbor data.
- *
- * @param neighbors Pointer to the received neighbor data.
- */
-void Controller::WeightedTopologyController::neighbors_callback(const WeightedTopologyNeighbors::SharedPtr &neighbors) {
-    if (!std::empty(neighbors->neighbors_position)) {
-        is_neighborhood_empty = false;
-        const auto vect_neighborhood{*neighbors};
-        neighbors_to_matrix(vect_neighborhood);
-    } else {
-        is_neighborhood_empty = true;
-    }
-}
-
-/**
-* @brief Timer callback function for periodic control computations.
+* @brief Constructor for WeightedTopologyNeighbors class.
 */
-void Controller::WeightedTopologyController::timer_callback() {
-    TrajectorySetpoint setpoint{};
-    if (!is_neighborhood_empty) {
-        // In fact, when there are no neighbors, there are no more messages published on the topic and
-        // is_neighborhood_empty remains locked at false.
-        is_neighborhood_empty = true;
-        publish_offboard_control_mode(CONTROL::ACCELERATION);
-        compute_command(setpoint);
-    } else {
-        command_tp = 0.0;
-        publish_offboard_control_mode(CONTROL::POSITION);
-        setpoint.position = {default_pose[0], default_pose[1], default_pose[2]};
-    }
-    setpoint.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    trajectory_setpoint_publisher_->publish(setpoint);
+Neighborhood::WeightedTopologyNeighbors::WeightedTopologyNeighbors()
+        : NearestNeighbors<custom_msgs::msg::WeightedTopologyNeighbors>() {
+    this->declare_parameter<std::vector<bool>>("leaders");
+    this->declare_parameter<std::vector<double>>("x_formation");
+    this->declare_parameter<std::vector<double>>("y_formation");
+    this->declare_parameter<std::vector<double>>("z_formation");
+////  Provide x_formation, y_formation and z_formation according to the NED frame
+    const auto x_formation{this->get_parameter("x_formation").as_double_array()};
+    const auto y_formation{this->get_parameter("y_formation").as_double_array()};
+    const auto z_formation{this->get_parameter("z_formation").as_double_array()};
+
+    vectors_to_Vector3d(x_formation, y_formation, z_formation);
+
+
+//    leaders = std::vector<bool>{true,false, false};
+    leaders = this->get_parameter("leaders").as_bool_array();
+    // Initialization provided in the Figure 2 of the paper. We assume they are not neighbors at the beginning.
+    prcs = PRCS::Constant(nb_drones, nb_drones);
+    prcs_neighborhood = PRCS::Constant(nb_drones, nb_drones);
 }
 
-
 /**
- * @brief Compute control command based on the current neighborhood information.
- *
- * @param setpoint Reference to the trajectory setpoint to be updated with the computed command.
+ * @brief Converts vectors representing x, y, and z coordinates to Eigen Vector3d objects.
+ * @param x The x-coordinates vector.
+ * @param y The y-coordinates vector.
+ * @param z The z-coordinates vector.
  */
-void Controller::WeightedTopologyController::compute_command(TrajectorySetpoint &setpoint) {
-    // TODO: Check North East Down in the neighbors when subtracting dij and here when sending in the command + DO UTest in debug with launching it int he launch file
-    const PoseTwist RPVVs{neighborhood.rowwise().sum()};
-    // [x, x_dot, y, y_dot, z, z_dot] => reshape to (2,3) [[-x,-x_dot],[-y,-y_dot],[-x,-z_dot]]
-    // Then sum the rows
-    const Eigen::RowVector3f command{-RPVVs.reshaped(2, 3).colwise().sum()};
-    // In offboard mode, if you want to send a command in acceleration you need to send nan on the position and the velocity
-    // See https://docs.px4.io/main/en/flight_modes/offboard.html
-    setpoint.position = {std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN(),
-                         std::numeric_limits<float>::quiet_NaN()};
-    setpoint.velocity = {std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN(),
-                         std::numeric_limits<float>::quiet_NaN()};
-    const auto now_tp{this->get_clock()->now().seconds()/1000};
-    auto dt{0.};
-    if (command_tp!=0){
-        dt = now_tp-command_tp;
-    }else{
-        pid_ax.reset();
-        pid_ay.reset();
-        pid_az.reset();
-        dt = command_tp;
+void
+Neighborhood::WeightedTopologyNeighbors::vectors_to_Vector3d(const std::vector<double> &x, const std::vector<double> &y,
+                                                             const std::vector<double> &z) {
+/**
+modified this bit to match C++17 formatting.
+*/
+    if (x.size() == nb_drones && y.size() == nb_drones && z.size() == nb_drones) {
+formation.reserve(nb_drones);
+        std::transform(std::begin(x), std::end(x), std::begin(formation), [&, idx = 0u](const auto aX) mutable {
+            Vector3d offset{aX, y[idx], z[idx]};
+            ++idx;
+            return offset;
+        });
+    } else {
+        RCLCPP_INFO(this->get_logger(), "Drone formation provided is not correct !");
     }
-    const auto x_updated_command{pid_ax.update(command[0],dt)};
-    const auto y_updated_command{pid_ay.update(command[1],dt)};
-    const auto z_updated_command{pid_az.update(command[2],dt)};
-    setpoint.acceleration = {x_updated_command, y_updated_command, z_updated_command};
-    command_tp = now_tp;
+
+}
+
+/**
+ * @brief Processes the position of a neighbor drone.
+ * @param drone_idx The index of the current drone.
+ * @param neighbor_idx The index of the neighbor drone.
+ * @param position The position of the current drone.
+ * @param neighbor_position The position of the neighbor drone.
+ * @param neighborhood The neighborhood message to be updated.
+ */
+void Neighborhood::WeightedTopologyNeighbors::process_neighbor_position(const std::size_t drone_idx,
+                                                                        const std::size_t neighbor_idx,
+                                                                        const VehicleLocalPosition &position,
+                                                                        VehicleLocalPosition neighbor_position,
+                                                                        WeightedTopologyNeighborsMsg &neighborhood) {
+    // Modify the neighborhood position with the inter-distance wanted between each drone
+    neighbor_position.x =
+            position.x - neighbor_position.x -
+            static_cast<float>(formation[drone_idx].x() - formation[neighbor_idx].x());
+    neighbor_position.y =
+            position.y - neighbor_position.y -
+            static_cast<float>(formation[drone_idx].y() - formation[neighbor_idx].y());
+    neighbor_position.z =
+            position.z - neighbor_position.z -
+            static_cast<float>(formation[drone_idx].z() - formation[neighbor_idx].z());
+    neighbor_position.vx = position.vx - neighbor_position.vx;
+    neighbor_position.vy = position.vy - neighbor_position.vy;
+    neighbor_position.vz = position.vz - neighbor_position.vz;
+
+    neighborhood.neighbors_position.emplace_back(neighbor_position);
+    neighborhood.neighbors_ids.emplace_back(neighbor_idx);
+    prcs_neighborhood[neighbor_idx] = prcs[neighbor_idx];
+}
+
+/**
+ * @brief Processes the neighborhood based on the positions of the neighbor drones.
+ * @param drone_idx The index of the current drone.
+ * @param neighborhood The neighborhood message to be updated.
+ */
+void Neighborhood::WeightedTopologyNeighbors::process_neighborhood(const std::size_t drone_idx,
+                                                                   WeightedTopologyNeighborsMsg &neighborhood) {
+    if (!std::empty(neighborhood.neighbors_position)) {
+        if (leaders[drone_idx]) {
+            prcs[drone_idx] = 1;
+        } else {
+            const auto min_prc_neighborhood{prcs_neighborhood.minCoeff() + 1};
+            prcs[drone_idx] = static_cast<std::size_t>(std::copysign(1u, min_prc_neighborhood)) *
+                              std::min(min_prc_neighborhood, nb_drones);
+            prcs_neighborhood.setConstant(nb_drones);
+        }
+    }
+}
+
+/**
+ * @brief Enriches the neighborhood message with weights calculated based on the PRCS (priority based on conflict state) of each neighbor.
+ * @param neighborhood The neighborhood message to be enriched.
+ */
+void Neighborhood::WeightedTopologyNeighbors::enrich_neighborhood(WeightedTopologyNeighborsMsg &neighborhood) {
+// When we arrive here, all the prcs have been updated
+    const auto sum_updated_prcs_neighborhood
+            {
+                    std::accumulate(std::begin(neighborhood.neighbors_ids), std::end(neighborhood.neighbors_ids),
+                                    0.0,
+                                    [this](const double sum, const auto id) {
+                                        return sum + 1. / static_cast<double>(prcs[id]);
+                                    })
+            };
+    Weights weights;
+    weights.reserve(nb_drones);
+    std::for_each(std::begin(neighborhood.neighbors_ids), std::end(neighborhood.neighbors_ids),
+                  [this, &weights, sum_updated_prcs_neighborhood](const auto id) {
+                      weights.emplace_back(
+                              (1. / static_cast<double>(prcs[id])) /
+                              static_cast<double>(sum_updated_prcs_neighborhood));
+                  });
+    neighborhood.set__weights(weights);
 }
 
 
 /**
- * @brief Main function to run the controller.
- *
- * Initializes the ROS 2 node and starts spinning the node.
- *
+ * @brief The main function that initializes and runs the WeightedTopologyNeighbors node.
  * @param argc Number of command-line arguments.
- * @param argv Command-line arguments.
+ * @param argv Array of command-line arguments.
  * @return int Exit status.
  */
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Controller::WeightedTopologyController>());
+    rclcpp::spin(std::make_shared<Neighborhood::WeightedTopologyNeighbors>());
     rclcpp::shutdown();
     return 0;
 }
